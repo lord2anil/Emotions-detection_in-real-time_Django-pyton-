70735cf37b83ad94cc6e7826317843022a399e37
lord2anil <a8208226@gmail.com>
2023-11-05 07:42:30 +0530

    upade the sockety to send video

--- emotion_detection/consumers.py
+++ emotion_detection/consumers.py
@@ -10,26 +10,62 @@ import asyncio
 class EmotionConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
-        self.cap = cv2.VideoCapture(-1)
+        # self.cap = cv2.VideoCapture(0)
         json_file = open("model/facialemotionmodel.json", "r")
         self.model_json = json_file.read()
         json_file.close()
         self.model = model_from_json(self.model_json)
         self.model.load_weights("model/facialemotionmodel.h5")
+        self.is_processing = False
+        self.chunks=[]
 
-    async def disconnect(self, close_code):
-        self.cap.release()
+    # async def disconnect(self, close_code):
+    #     self.cap.release()
 
-    async def receive(self, text_data):
-        # print(text_data)
-        if text_data == "start":
-            self.is_processing = True
-            await self.process_video()
-        
-        elif text_data == "stop":
-            # print("stop")   
-            # await self.is_processing = False
-            await self.stop_video_processing()
+    async def receive(self, text_data=None, bytes_data=None, **kwargs):
+          labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
+          
+          if(text_data):
+            
+         
+
+            self.chunks.append( text_data)
+            # print(len(self.chunks))
+            if len(self.chunks) > 0:
+                frame_str = self.chunks.pop(0)
+                decodeit = open('hello_level.jpeg', 'wb') 
+                decodeit.write(base64.b64decode((frame_str))) 
+                decodeit.close() 
+                  #working 
+
+
+                frame = cv2.imdecode(np.frombuffer(base64.b64decode(frame_str), np.uint8), cv2.IMREAD_COLOR)
+
+                # Convert the frame to grayscale
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                
+                
+                faces = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml').detectMultiScale(frame, 1.3, 5)
+                try:
+                    for (x, y, w, h) in faces:
+                        face = gray[y:y + h, x:x + w]
+                        face = cv2.resize(face, (48, 48))
+                        face = face.reshape(1, 48, 48, 1) / 255.0
+                        prediction = self.model.predict(face)
+                        emotion = labels[np.argmax(prediction)]
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                        cv2.putText(frame, emotion, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                    _, buffer = cv2.imencode('.jpg', frame)
+                    frame_bytes = buffer.tobytes()  # Convert to bytes
+                    await self.send(bytes_data=frame_bytes)
+                    # await self.send(text_data="Hello world!")
+                    
+                except Exception as e:
+                    print("Error in processing video:", e)
+                # await self.send(text_data="Hello world2!")
+                # await asyncio.sleep(0.01)
+                
+            
     
     async def stop_video_processing(self):
         self.is_processing = False
@@ -39,12 +75,19 @@ class EmotionConsumer(AsyncWebsocketConsumer):
 
   
     async def process_video(self):
+        print(len(self.chunks))
+
         labels = {0: 'angry', 1: 'disgust', 2: 'fear', 3: 'happy', 4: 'neutral', 5: 'sad', 6: 'surprise'}
         # await self.send(text_data="Hello world!")
-        while self.is_processing:
-            success, frame = self.cap.read()
-            if not success:
-                break
+        if len(self.chunks) > 0:
+            # success, frame = self.cap.read()
+            frame_base64 = self.chunks.pop(0)
+            print(frame_base64)
+            frame_bytes = base64.b64decode(frame_base64)
+
+            # if not success:
+            #     break
+            frame = cv2.imdecode(np.frombuffer(frame_bytes, np.uint8), cv2.IMREAD_COLOR)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml').detectMultiScale(frame, 1.3, 5)
             try:
